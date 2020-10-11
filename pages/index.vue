4b51ff153a715416954a0a87f53e598aaa4fa5d6
miyuta <yuemiyuta@gmail.com>
2020-10-12 02:40:49 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline"> Hello </v-card-title>
+        <v-card-text>
+          <p>hello</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
